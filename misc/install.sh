3b74a6f54199b178d3c5c8195c39e0f0e25cf315
J-Dog <j-dog@j-dog.net>
2018-03-15 10:56:29 -0700

    ubuntu URI fixes
    - fixes issue with URIs not working on linux/ubuntu (https://github.com/jdogresorg/freewallet/issues/15)

--- misc/install.sh
+++ misc/install.sh
@@ -7,13 +7,15 @@ echo "### Creating installation directories..."
 mkdir -vp $basedir/FreeWallet
 mkdir -vp $basedir/icons/FreeWallet
 mkdir -vp $basedir/applications
-echo "### Installing FreeWallet ..."
+echo "### Installing FreeWallet..."
 cp -a * $basedir/FreeWallet/
 cp FreeWallet.png $basedir/icons/FreeWallet/
-echo "### Integrating FreeWallet with linux desktop ..."
+echo "### Integrating FreeWallet with linux desktop..."
 cp FreeWallet.desktop $basedir/applications
-echo "### Setting file permissions ..."
+echo "### Setting file permissions..."
 chmod 644 $basedir/icons/FreeWallet/FreeWallet.png 
 chmod 644 $basedir/applications/FreeWallet.desktop
 chmod 755 $basedir/FreeWallet/*
+echo "### Updating desktop database..."
+/usr/bin/update-desktop-database
 echo "### Installation Complete!"
